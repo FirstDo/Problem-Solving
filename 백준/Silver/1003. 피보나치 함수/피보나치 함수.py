90307d2d62117173de2a64cfbd9673feca759d87
DuDu <69573768+FirstDo@users.noreply.github.com>
2022-03-11 16:02:06 +0900

    [Silver III] Title: 피보나치 함수, Time: 64 ms, Memory: 28776 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+
+T=int(input())
+fibo=[[0,0] for _ in range(41)]
+fibo[0]=[1,0]
+fibo[1]=[0,1]
+for i in range(2,41):
+    fibo[i]=[fibo[i-2][0]+fibo[i-1][0],fibo[i-2][1]+fibo[i-1][1]]
+
+
+for case in range(T):
+    n=int(input())
+    print(fibo[n][0],fibo[n][1])
+    
+
