70728a3d5e13e4eafb521c8ba314e87e48de88d1
DuDu <69573768+FirstDo@users.noreply.github.com>
2022-03-11 21:03:11 +0900

    [Gold IV] Title: 문자열 폭발, Time: 284 ms, Memory: 117508 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.swift
@@ -0,0 +1,23 @@
+//9935번 문자열 폭발
+let input = Array(readLine()!).map{String($0)}
+let target = readLine()!
+let targetLen = target.count
+
+let lastCh = String(target.last!)
+var stack = [String]()
+
+for ch in input {
+    stack.append(ch)
+    
+    if ch == lastCh {
+        let len = stack.count
+        if len >= targetLen && stack[(len-targetLen)...].joined() == target{
+            (0..<targetLen).forEach{_ in
+                stack.removeLast()
+            }
+        }
+    }
+}
+
+let ans = stack.joined()
+print(ans.isEmpty ? "FRULA" : ans)
