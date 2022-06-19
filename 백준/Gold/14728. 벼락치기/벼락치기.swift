53b6d995e9d36bb15b3ee234dc85c650f030c6e6
DuDu <whqtkf12@naver.com>
2022-06-20 06:21:35 +0900

    [Gold V] Title: 벼락치기, Time: 32 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.swift
+++ 백준/Gold/14728. 벼락치기/벼락치기.swift
@@ -5,25 +5,16 @@ import Foundation
 let T = readLine()!.components(separatedBy: " ").map {Int($0)!}
 let (n, t) = (T[0], T[1])
 
-let list = (0..<n).compactMap { _ -> (weight: Int, value: Int) in
+var dp = Array(repeating: 0, count: 10001)
+
+for _ in 0..<n {
     let T = readLine()!.components(separatedBy: " ").map {Int($0)!}
-    return (T[0], T[1])
-}
-
-var dp = Array(repeating: Array(repeating: 0, count: 10001), count: n)
-
-for k in 0...t where k >= list[0].weight {
-    dp[0][k] = list[0].value
-}
-
-for i in 1..<n {
-    for j in 0...t {
-        if j - list[i].weight >= 0 {
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j - list[i].weight] + list[i].value)
-        } else {
-            dp[i][j] = dp[i-1][j]
-        }
+    let (weight, value) = (T[0], T[1])
+    
+    for i in stride(from: t, through: weight, by: -1) {
+        dp[i] = max(dp[i], dp[i - weight] + value)
     }
 }
 
-print(dp[n-1][t])
+print(dp[t])
+    
