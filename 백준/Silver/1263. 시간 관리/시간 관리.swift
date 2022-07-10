9b9a38d2505e0b5e5ef093f4f430a08fc2684347
DuDu <whqtkf12@naver.com>
2022-07-10 19:20:19 +0900

    [Silver I] Title: 시간 관리, Time: 16 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Silver/1263. 시간 관리/시간 관리.swift
+++ 백준/Silver/1263. 시간 관리/시간 관리.swift
@@ -14,27 +14,19 @@ for _ in 0..<Int(readLine()!)! {
     works.append(Work(time: T[0], deadLine: T[1]))
 }
 
-// 데드라인이 급한일부터 처리
 works.sort { lhs, rhs in
-    lhs.deadLine < rhs.deadLine
+    lhs.deadLine > rhs.deadLine
 }
 
-var answer = -1
+var deadLine = works.first!.deadLine
 
-// 0부터 1_000_000까지 완전탐색
-TimeLoop: for startTime in 0...1_000_000 {
-    var current = startTime
-    
-    for work in works {
-        current += work.time
-        
-        // 데드라인을 넘긴다면 끝!
-        if current > work.deadLine {
-            break TimeLoop
-        }
-    }
-    
-    answer = startTime
+for work in works {
+    deadLine = min(deadLine, work.deadLine)
+    deadLine -= work.time
 }
 
-print(answer)
+if deadLine < 0 {
+    print(-1)
+} else {
+    print(deadLine)
+}
