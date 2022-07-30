f261df102da255d02fd37dc859c5aeb8618009db
DuDu <whqtkf12@naver.com>
2022-07-30 17:13:38 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 5.03 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+func solution(_ left:Int, _ right:Int) -> Int {
+    return (left...right).map { n in
+        (1...n).filter { n % $0 == 0}.count % 2 == 0 ? n : -n
+    }
+    .reduce(0, +)
+}
