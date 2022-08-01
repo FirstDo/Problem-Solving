5cfc7bc7c815b84892f20d8d4ca6053621022f7b
DuDu <whqtkf12@naver.com>
2022-08-01 22:19:34 +0900

    [level 2] Title: [3차] 압축, Time: 0.35 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.swift
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.swift
@@ -3,11 +3,10 @@ import Foundation
 var dict: [String: Int] = ["A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12, "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17, "R": 18, "S": 19, "T": 20, "U": 21, "V": 22, "W": 23, "X": 24, "Y": 25, "Z": 26]
 
 func solution(_ msg:String) -> [Int] {
-    var dictIndex = 27
-    
     let msg = Array(msg)
     let len = msg.count
     
+    var dictIndex = 27
     var result = [Int]()
     
     var i = 0
@@ -16,31 +15,25 @@ func solution(_ msg:String) -> [Int] {
         // 사전에 있는 최대 범위까지 커버하기
         var current = "\(msg[i])"
         
-        if i == len - 1 {
-            result.append(dict[current]!)
-            break
-        }
-        
-        for j in i+1..<len {
-            current.append(msg[j])
-            
-            if dict[current] == nil {
-                // 새 문자열을 사전에 넣기
+        while true {
+            // 문자열이 사전에 있을 경우. key 문자열 +1
+            if dict[current] != nil {
+                i += 1
+                
+                if i == len {
+                    result.append(dict[current]!)
+                    break
+                }
+                
+                current.append(msg[i])
+            }
+            // 문자열이 사전에 없을경우, 사전에 추가
+            else {
                 dict[current] = dictIndex
                 dictIndex += 1
-                
-                // result 배열에 추가
                 _ = current.removeLast()
                 result.append(dict[current]!)
-                
-                i = j
-                
                 break
-            }
-            
-            if j == len - 1 {
-                result.append(dict[current]!)
-                i = len
             }
         }
     }
