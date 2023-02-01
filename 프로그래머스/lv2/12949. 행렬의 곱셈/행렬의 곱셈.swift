061d23413ad92612d31d0ca52a87479b44cf1735
DuDu <whqtkf12@naver.com>
2023-02-01 16:56:38 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.88 ms, Memory: 16.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.swift
@@ -0,0 +1,17 @@
+func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+    
+    let row = arr1.count
+    let col = arr2[0].count
+    
+    var result = Array(repeating: Array(repeating: 0, count: col), count: row)
+    
+    for i in 0..<row {
+        for j in 0..<col {
+            for k in 0..<arr1[0].count {
+                result[i][j] += arr1[i][k] * arr2[k][j]
+            }
+        }
+    }
+    
+    return result
+}
