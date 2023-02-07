b08f74e82d84b8e6012a794295a9dc7f5aee5e3b
DuDu <whqtkf12@naver.com>
2023-02-07 16:33:03 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.08 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.swift
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.swift
@@ -1,27 +1,23 @@
 import Foundation
 
-func isPrime(_ num: Int) -> Bool {
-    switch num {
-    case 0,1:
-        return false
-    case 2,3:
-        return true
-    default:
-        break
+private func isPrime(_ n: Int) -> Bool {
+    switch n {
+    case 0, 1: return false
+    case 2, 3: return true
+    default: break
     }
     
-    for i in 2...Int(sqrt(Double(num))) {
-        if num % i == 0 { return false }
+    for i in 2...Int(sqrt(Double(n))) {
+        if n % i == 0 { return false }
     }
     
     return true
 }
 
 func solution(_ n:Int, _ k:Int) -> Int {
-    let number = String(n, radix: k, uppercase: true).components(separatedBy: "0").compactMap { num -> Int? in
-        if num.isEmpty { return nil }
-        return Int(num)
-    }
-
-    return number.filter(isPrime(_:)).count
+    return String(n, radix: k)
+        .components(separatedBy: "0")
+        .compactMap { Int($0) }
+        .filter(isPrime)
+        .count
 }
