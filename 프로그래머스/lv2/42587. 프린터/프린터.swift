8b4c27b5a760fd967744f0f9402608ff83847734
DuDu <whqtkf12@naver.com>
2023-02-07 15:12:46 +0900

    [level 2] Title: 프린터, Time: 0.43 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.swift
+++ 프로그래머스/lv2/42587. 프린터/프린터.swift
@@ -1,69 +1,53 @@
 import Foundation
 
-final class Queue<T> {
-    var input = [T]()
-    var output = [T]()
-    
-    var count: Int {
-        return input.count + output.count
-    }
+final class Queue<Element> {
+    var input = [Element]()
+    var output = [Element]()
     
     var isEmpty: Bool {
-        return input.isEmpty && output.isEmpty
+        input.isEmpty && output.isEmpty
     }
     
-    func enqueue(_ element: T) {
-        input.append(element)
-    }
-    
-    @discardableResult
-    func dequeue() -> T {
+    var front: Element {
         if output.isEmpty {
             output = input.reversed()
             input.removeAll()
         }
         
-        return output.removeLast()
+        return output.last!
+    }
+    
+    func enQueue(_ element: Element) {
+        input.append(element)
+    }
+    
+    func deQueue() -> Element? {
+        if output.isEmpty {
+            output = input.reversed()
+            input.removeAll()
+        }
+        
+        return output.popLast()
     }
 }
 
+
 func solution(_ priorities:[Int], _ location:Int) -> Int {
-    // key: 작업 우선순위, value: 해당 작업보다 우선순위가 높은 작업의 개수
-    var dict = [Int:Int]()
+    var number = 0
+    let queue = Queue<(id: Int, priority: Int)>()
     
-    Set(priorities).forEach { num in
-        dict[num] = priorities.filter { $0 > num }.count
-    }
-    
-    var index = location
-    var cnt = 0
-    
-    let queue = Queue<Int>()
-    priorities.forEach { queue.enqueue($0) }
+    priorities.enumerated().forEach { queue.enQueue(($0.offset, $0.element)) }
     
     while !queue.isEmpty {
-        let now = queue.dequeue()
+        let cur = queue.deQueue()!
         
-        // 더 중요한게 없다면 인쇄!
-        if dict[now] == 0 {
-            cnt += 1
-            // 작업 하나가 빠졌으므로, 딕셔너리 - 1 해주기
-            for key in dict.keys where key < now {
-                dict[key]! -= 1
-            }
-            
-            // 현재 인쇄한 작업이 타겟이라면..
-            if index == 0 {
-                return cnt
-            }
+        if (queue.input + queue.output).contains(where: { $0.priority > cur.priority }) {
+            queue.enQueue(cur)
+        } else {
+            number += 1
+            if cur.id == location { break }
         }
-        // 더 중요한게 남아 있다면 queue의 맨끝에 다시 넣어주기
-        else {
-            queue.enqueue(now)
-        }
-        
-        index = ((index - 1) + queue.count) % queue.count
     }
     
-    return 0
+    return number
 }
