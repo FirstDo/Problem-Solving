71b0153f65369b75a3c02b1077ce0b01e0ace4c6
DuDu <whqtkf12@naver.com>
2023-02-08 14:21:00 +0900

    [level 2] Title: [3차] 압축, Time: 0.99 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.swift
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.swift
@@ -1,40 +1,43 @@
-import Foundation
-
-var dict: [String: Int] = ["A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12, "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17, "R": 18, "S": 19, "T": 20, "U": 21, "V": 22, "W": 23, "X": 24, "Y": 25, "Z": 26]
+var dicts: [String: Int] = [
+    "A": 1, "B": 2, "C": 3,
+    "D": 4, "E": 5, "F": 6,
+    "G": 7, "H": 8, "I": 9,
+    "J": 10, "K": 11, "L": 12,
+    "M": 13, "N": 14, "O": 15,
+    "P": 16, "Q": 17, "R": 18,
+    "S": 19, "T": 20, "U": 21,
+    "V": 22, "W": 23, "X": 24,
+    "Y": 25, "Z": 26
+]
 
 func solution(_ msg:String) -> [Int] {
-    let msg = Array(msg)
-    let len = msg.count
+    if dicts[msg] != nil { return [dicts[msg]!] }
     
-    var dictIndex = 27
+    var msg = msg.map { String($0) }
+    
+    var startIndex = 0
+    var endIndex = 0
+    
     var result = [Int]()
     
-    var i = 0
-    
-    while i < len {
-        // 사전에 있는 최대 범위까지 커버하기
-        var current = "\(msg[i])"
+    while true {
+        if endIndex == msg.count {
+            result.append(dicts[msg[startIndex..<endIndex].joined()]!)
+            break
+        }
         
-        while true {
-            // 문자열이 사전에 있을 경우. key 문자열 +1
-            if dict[current] != nil {
-                i += 1
-                
-                if i == len {
-                    result.append(dict[current]!)
-                    break
-                }
-                
-                current.append(msg[i])
-            }
-            // 문자열이 사전에 없을경우, 사전에 추가
-            else {
-                dict[current] = dictIndex
-                dictIndex += 1
-                _ = current.removeLast()
-                result.append(dict[current]!)
-                break
-            }
+        let targetKey = msg[startIndex...endIndex].joined()
+        
+        if dicts[targetKey] == nil {
+            dicts[targetKey] = dicts.count + 1
+
+            let key = msg[startIndex...endIndex-1].joined()
+            result.append(dicts[key]!)
+            
+            startIndex = endIndex
+            endIndex = startIndex
+        } else {
+            endIndex += 1
         }
     }
     
