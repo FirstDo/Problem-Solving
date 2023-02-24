127a6534d70447cf31aa8497f0aa6f6af98d4547
DuDu <whqtkf12@naver.com>
2023-02-24 14:25:39 +0900

    [level 1] Title: 모의고사, Time: 6.35 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.swift
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.swift
@@ -1,19 +1,15 @@
-import Foundation
-
 func solution(_ answers:[Int]) -> [Int] {
-    let answer = (
-        a: [1, 2, 3, 4, 5],
-        b: [2, 1, 2, 3, 2, 4, 2, 5],
-        c: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    )
+
+    let compareFirst = [1,2,3,4,5]
+    let compareSecond = [2,1,2,3,2,4,2,5]
+    let compareThird = [3,3,1,1,2,2,4,4,5,5]
     
-    var result = [1: 0, 2: 0, 3: 0]
+    let first = answers.enumerated().filter{$0.element == compareFirst[$0.offset % compareFirst.count]}.count
+    let second = answers.enumerated().filter{$0.element == compareSecond[$0.offset % compareSecond.count]}.count
+    let third = answers.enumerated().filter{$0.element == compareThird[$0.offset % compareThird.count]}.count
     
-    for (index, value) in answers.enumerated() {
-        if value == answer.a[index % 5] { result[1]! += 1 }
-        if value == answer.b[index % 8] { result[2]! += 1 }
-        if value == answer.c[index % 10] { result[3]! += 1 }
-    }
+    
+    let result = [1: first, 2: second, 3: third]
     
     return result
         .sorted { $0.key < $1.key }
