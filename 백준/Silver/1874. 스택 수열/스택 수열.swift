2f75e48e13013b729d85521039b00abecce50c5b
DuDu <whqtkf12@naver.com>
2024-01-23 14:38:06 +0900

    [Silver II] Title: 스택 수열, Time: 1896 ms, Memory: 86980 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+let n = Int(readLine()!)!
+var target = [Int]()
+
+for _ in 0..<n {
+  let line = Int(readLine()!)!
+  target.append(line)
+}
+
+print(makeSequence())
+
+func makeSequence() -> String {
+  var temp = [String]()
+  var stack = [Int]()
+
+  // 1부터 n까지 반복
+  for i in 1...n {
+    // 무조건 push
+    stack.append(i)
+    temp.append("+")
+    
+    // stack의 top이 target이면 pop
+    while stack.last == target.first && !stack.isEmpty {
+      _ = stack.removeLast()
+      _ = target.removeFirst()
+      
+      temp.append("-")
+    }
+  }
+  
+  
+  if stack.isEmpty && target.isEmpty {
+    return temp.joined(separator: "\n")
+  } else {
+    return "NO"
+  }
+}
