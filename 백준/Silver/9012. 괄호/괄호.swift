e138b090665f20bd1322ed581e16b5e4536a8920
DuDu <whqtkf12@naver.com>
2024-01-23 14:04:32 +0900

    [Silver IV] Title: 괄호, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+for _ in 0..<n {
+  let line = readLine()!
+  
+  if isCorrectParenthesis(line) {
+    print("YES")
+  } else {
+    print("NO")
+  }
+}
+
+func isCorrectParenthesis(_ input: String) -> Bool {
+  let array = Array(input)
+  
+  var stack = [Character]()
+  
+  for ch in array {
+    // ( 이면 스택에 push
+    if ch == "(" {
+      stack.append(ch)
+    } 
+    // ) 이면 -> 스택비어있으면 에러, 스택의 top에 (없으면 에러, 문제없으면 pop
+    else {
+      if stack.last != "(" { return false }
+      _ = stack.removeLast()
+    }
+  }
+  
+  return stack.isEmpty
+}
