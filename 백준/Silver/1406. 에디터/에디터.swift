875759ebfa84ee8d6255ea7ff1c17bb738300635
DuDu <whqtkf12@naver.com>
2024-01-24 09:26:33 +0900

    [Silver II] Title: 에디터, Time: 284 ms, Memory: 113916 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+let str = readLine()!
+let m = Int(readLine()!)!
+
+var leftStack: [Character] = Array(str)
+var rightStack = [Character]()
+
+for _ in 0..<m {
+  let input = readLine()!.last!
+  
+  switch input {
+  case "L":
+    if leftStack.isEmpty { break }
+    
+    rightStack.append(leftStack.removeLast())
+    
+  case "D":
+    if rightStack.isEmpty { break }
+    
+    leftStack.append(rightStack.removeLast())
+  
+  case "B":
+    if leftStack.isEmpty { break }
+    
+    leftStack.removeLast()
+    
+  default:
+    leftStack.append(input)
+  }
+}
+
+print((leftStack + rightStack.reversed()).map { String($0) }.joined(separator: ""))
