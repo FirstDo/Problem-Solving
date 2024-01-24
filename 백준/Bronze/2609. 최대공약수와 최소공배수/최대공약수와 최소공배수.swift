a9651146ae82317635a42536933b78b2c21289ee
DuDu <whqtkf12@naver.com>
2024-01-24 10:38:09 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func getGCD(a: Int, b: Int) -> Int {
+  var (a, b) = (a, b)
+  
+  while b != 0 {
+    let r = a % b
+    a = b
+    b = r
+  }
+  
+  return a
+}
+
+func getLCM(a: Int, b: Int) -> Int {
+  return a * b / getGCD(a: a, b: b)
+}
+
+let input = readLine()!.components(separatedBy: " ").map { Int($0)! }
+
+let (a,b) = (input[0], input[1])
+
+print(getGCD(a: a, b: b))
+print(getLCM(a: a, b: b))
