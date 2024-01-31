c7528f0907c8b967497595d0249a3b346b221326
DuDu <whqtkf12@naver.com>
2024-01-31 12:36:37 +0900

    [Bronze II] Title: 소수 찾기, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.swift
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.swift
@@ -3,22 +3,22 @@ import Foundation
 // 에라토스테네스의 체 사용
 func allPrimes(_ max: Int) -> [Bool] {
   let limit = [max+1, 100].max()!
-  
+
   var temp = Array(repeating: true, count: limit + 1)
   temp[0] = false
   temp[1] = false
-  
+
   for i in 2 ..< Int(sqrt(Double(limit))) + 1 {
     if temp[i] {
       var j = 2
-      
+
       while i * j <= limit {
         temp[i * j] = false
         j += 1
       }
     }
   }
-  
+
   return temp
 }
 
