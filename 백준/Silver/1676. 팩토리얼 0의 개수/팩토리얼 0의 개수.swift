f95a2e6c93cb170fd3906f20c9bb84310446599d
DuDu <whqtkf12@naver.com>
2024-01-31 12:37:36 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.swift
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.swift
@@ -6,13 +6,13 @@ var num5 = 0
 
 for i in 1...n {
   var temp = i
-  
+
   // i에 2가 몇번들어있는지 카운트
   while temp % 2 == 0 {
     temp = temp / 2
     num2 += 1
   }
-  
+
   // i에 5가 몇번 들어있는지 카운트
   while temp % 5 == 0 {
     temp = temp / 5
