52add512e23d07994a0b0734666f15037501c654
DuDu <whqtkf12@naver.com>
2024-01-31 12:34:23 +0900

    [Silver II] Title: 쇠막대기, Time: 20 ms, Memory: 82796 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.swift
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.swift
@@ -6,7 +6,7 @@ var stack = [Character]()
 var result = 0
 
 for (index, ch) in str.enumerated() {
-  
+
   // 레이저일 경우 자르기
   if ch == ")" && str[index-1] == "(" {
     stack.removeLast() // stack에 들어간 레이저: ( 빼주기
