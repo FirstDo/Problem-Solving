291828090593d81664f1d61f5e515c885f580c57
DuDu <whqtkf12@naver.com>
2024-02-06 14:47:43 +0900

    [Silver III] Title: 2×n 타일링, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+let n = Int(readLine()!)!
+let mod = 10_007
+
+var dp = Array(repeating: 0, count: n+2)
+
+dp[1] = 1
+dp[2] = 2
+
+for i in 1..<n {
+  dp[i+2] = (dp[i+1] + dp[i]) % mod
+}
+
+print(dp[n])
