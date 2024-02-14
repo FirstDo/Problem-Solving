3273d3323b71eb3bc34d107169afcaa81ac44858
DuDu <whqtkf12@naver.com>
2024-02-14 13:04:42 +0900

    [Silver II] Title: N과 M (9), Time: 36 ms, Memory: 72188 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.swift
@@ -0,0 +1,35 @@
+// 순열
+func permutation(arr: [Int], r: Int) -> [[Int]] {
+  var result = [[Int]]()
+  var visit = Array(repeating: false, count: arr.count)
+  cycle([])
+  
+  func cycle(_ temp: [Int]) {
+    if temp.count == r {
+      result.append(temp)
+      return
+    }
+    
+    var previousNum = 0
+    
+    for i in 0..<arr.count {
+      if visit[i] || previousNum == arr[i] {
+        continue
+      }
+      
+      previousNum = arr[i]
+      visit[i] = true
+      cycle(temp + [arr[i]])
+      visit[i] = false
+    }
+  }
+  
+  return result
+}
+
+let nm = readLine()!.split(separator: " ").map { Int(String($0))! }
+let (n,m) = (nm[0], nm[1])
+let arr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+
+let results = permutation(arr: arr, r: m)
+print(results.map { $0.map { String($0) }.joined(separator: " ")}.joined(separator: "\n"))
