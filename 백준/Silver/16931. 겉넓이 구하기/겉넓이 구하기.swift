385c7b9b63074dc69c7cd3a879bcf0672a4f8e23
DuDu <whqtkf12@naver.com>
2024-02-29 16:56:26 +0900

    [Silver II] Title: 겉넓이 구하기, Time: 16 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.swift
+++ 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.swift
@@ -1,35 +1,56 @@
-// 16931번 겉넓이 구하기
-
 import Foundation
 
-let dx = [1,-1,0,0]
-let dy = [0,0,1,-1]
+let nm = readLine()!.split(separator: " ").map {Int($0)!}
+let (n, m) = (nm[0], nm[1])
 
-let t = readLine()!.components(separatedBy: " ").map{Int($0)!}
-let (n, m) = (t[0], t[1])
+let shape = (0..<n).map { _ in readLine()!.split(separator: " ").map {Int($0)!} }
 
-var arr = Array(repeating: Array(repeating: 0, count: m+2), count: n+2)
+var cnt = 0
 
-for i in 1...n {
-    arr[i] = [0] + readLine()!.components(separatedBy: " ").map{Int($0)!} + [0]
-}
-
-var result = 0
-
-// 상, 하 겉넓이 더하기
-result += n * m * 2
-
-// 한 자리에 대해서 옆면 너비 더하기
-for i in 1...n {
-    for j in 1...m {
-        for k in 0..<4 {
-            let nx = dx[k] + i
-            let ny = dy[k] + j
-            
-            let value = arr[i][j] - arr[nx][ny]
-            if value > 0 { result += value }
-        }
+for i in 0..<n {
+  for j in 0..<m {
+    if j == 0 {
+      cnt += shape[i][j]
     }
+    
+    if j+1 < m && shape[i][j+1] > shape[i][j] {
+      cnt += shape[i][j+1] - shape[i][j]
+    }
+  }
+  
+  for j in stride(from: m-1, to: -1, by: -1) {
+    if j == m-1 {
+      cnt += shape[i][j]
+    }
+    
+    if j-1 >= 0 && shape[i][j-1] > shape[i][j] {
+      cnt += shape[i][j-1] - shape[i][j]
+    }
+  }
 }
 
-print(result)
+for j in 0..<m {
+  for i in 0..<n {
+    if i == 0 {
+      cnt += shape[i][j]
+    }
+    
+    if i+1 < n && shape[i+1][j] > shape[i][j] {
+      cnt += shape[i+1][j] - shape[i][j]
+    }
+  }
+  
+  for i in stride(from: n-1, to: -1, by: -1) {
+    if i == n-1 {
+      cnt += shape[i][j]
+    }
+    
+    if i-1 >= 0 && shape[i-1][j] > shape[i][j] {
+      cnt += shape[i-1][j] - shape[i][j]
+    }
+  }
+}
+
+cnt += n * m * 2
+
+print(cnt)
