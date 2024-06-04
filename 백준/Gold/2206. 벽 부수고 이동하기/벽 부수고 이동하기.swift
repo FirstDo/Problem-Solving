b3e3fa4bf752b449a8c8f1d452a26e4ff1167e7d
DuDu <whqtkf12@naver.com>
2024-06-04 11:02:59 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 544 ms, Memory: 241416 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.swift
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.swift
@@ -1,46 +1,52 @@
 import Foundation
-//2206번 벽 부수고 이동하기
 
-let t = readLine()!.split(separator: " ").map{Int(String($0))!}
-let (N,M) = (t[0],t[1])
-
-var graph = [[Int]]()
-
-for _ in 0..<N {
-	let t = Array(readLine()!).map{Int(String($0))!}
-	graph.append(t)
+func bfs(_ n: Int, _ m: Int, maps: [[Int]]) -> Int {
+  let dx = [1,-1,0,0]
+  let dy = [0,0,1,-1]
+  
+  var queue = [(x: Int, y: Int, wall: Int, cnt: Int)]()
+  queue.append((0,0,0,0))
+  var index = 0
+  
+  var visit = Array(repeating: Array(repeating: Array(repeating: false, count: 2), count: m), count: n)
+  visit[0][0][0] = true
+  
+  while queue.count > index {
+    let (x, y, wall, cnt) = queue[index]
+    index += 1
+    
+    if x == n-1 && y == m-1 {
+      return cnt + 1
+    }
+    
+    for i in 0..<4 {
+      let (nx, ny) = (x + dx[i], y + dy[i])
+      
+      if (0..<n) ~= nx && (0..<m) ~= ny {
+        // 벽 아니고, 이동가능하면
+        if maps[nx][ny] == 0 && visit[nx][ny][wall] == false {
+          visit[nx][ny][wall] = true
+          queue.append((nx, ny, wall, cnt + 1))
+        }
+        
+        // 벽이고, 뚫을 수 있고, 이동가능하면
+        if maps[nx][ny] == 1 && wall == 0 && visit[nx][ny][wall+1] == false {
+          visit[nx][ny][wall + 1] = true
+          queue.append((nx, ny, wall + 1, cnt + 1))
+        }
+      }
+    }
+  }
+  
+  return -1
 }
 
-let dx = [1,-1,0,0]
-let dy = [0,0,1,-1]
-
-func bfs() -> Int {
-	var queue = [(0,0,1)]
-	var visit = Array(repeating: Array(repeating: [0,0], count: M), count: N)
-	visit[0][0][1] = 1
-	var idx = 0
-
-	while queue.count > idx {
-		let (x,y,w) = queue[idx]
-		idx += 1
-
-		if x == N-1 && y == M-1 {
-			return visit[x][y][w]
-		}
-
-		for i in 0..<4 {
-			let (nx,ny) = (dx[i]+x, dy[i]+y)
-			if (0..<N).contains(nx) && (0..<M).contains(ny) {
-				if graph[nx][ny] == 1 && w == 1 {
-					visit[nx][ny][0] = visit[x][y][1] + 1
-					queue.append((nx,ny,0))
-				} else if graph[nx][ny] == 0 && visit[nx][ny][w] == 0 {
-					visit[nx][ny][w] = visit[x][y][w] + 1
-					queue.append((nx,ny,w))
-				}
-			}
-		}
-	}
-	return -1
+func start() {
+  let nm = readLine()!.components(separatedBy: " ").map {Int($0)!}
+  let (n, m) = (nm[0], nm[1])
+  let maps = (0..<n).map { _ in Array(readLine()!).map {Int(String($0))! } }
+  let result = bfs(n, m, maps: maps)
+  print(result)
 }
-print(bfs())
+
+start()
