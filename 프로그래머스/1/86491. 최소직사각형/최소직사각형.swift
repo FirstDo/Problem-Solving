1d64d547de360d05818a869d0b460bbd5e2fbecf
DuDu <whqtkf12@naver.com>
2024-10-30 21:07:34 +0900

    [level 1] Title: 최소직사각형, Time: 10.21 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
@@ -1,7 +1,8 @@
 import Foundation
 
 func solution(_ sizes:[[Int]]) -> Int {
-  let width = sizes.flatMap { $0 }.max()!
-  let height = sizes.map { $0.min()! }.max()!
-  return width * height
+  let maxWidth = sizes.flatMap { $0 }.max()!
+  let maxHeight = sizes.map { $0.min()! }.max()!
+  
+  return maxWidth * maxHeight
 }
