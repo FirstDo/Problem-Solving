676f1ac3078fc5e3dcf394673487bcee3545e7af
DuDu <whqtkf12@naver.com>
2024-10-31 11:30:59 +0900

    [level 2] Title: 카펫, Time: 0.15 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.swift
+++ 프로그래머스/2/42842. 카펫/카펫.swift
@@ -1,14 +1,16 @@
 import Foundation
 
 func solution(_ brown:Int, _ yellow:Int) -> [Int] {
+  // a * b = yellow
+  // a + b = (brown - 4)/2
   
-  for height in 1...yellow where yellow % height == 0 {
-    let width = yellow / height
-
-    if (width + 2) * (height + 2) == brown + yellow {
-      return [width + 2, height + 2]
+  // a: 가로의 길이, b: 세로의 길이
+  for a in 1...2_000_000 {
+    let b = (brown - 4)/2 - a
+    
+    if a*b == yellow {
+      return [a+2, b+2].sorted(by: >)
     }
   }
-  
   return []
 }
