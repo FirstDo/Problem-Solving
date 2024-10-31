579d74c318fff5f72579488a904e4e420098a366
DuDu <whqtkf12@naver.com>
2024-10-31 12:31:39 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 10.06 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.swift
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.swift
@@ -1,49 +1,42 @@
 import Foundation
 
 func solution(_ n:Int, _ wires:[[Int]]) -> Int {
-  // Method
-  func bfs(_ start: Int, disabledNode: Int) -> Int{
-    var index = 0
-    var queue = [start]
-    var visit = Array(repeating: false, count: n)
-    
-    visit[start] = true
-    visit[disabledNode] = true
-    var cnt = 1 // 방문한 노드의 개수
-    
-    while index < queue.count {
-      let curNode = queue[index]
-      index += 1
-      
-      for nextNode in graph[curNode] where visit[nextNode] == false {
-        cnt += 1
-        visit[nextNode] = true
-        queue.append(nextNode)
-      }
-    }
-    
-    return cnt
-  }
-  
-  // Logic
-  
-  var answer = n
-  // 그래프 생성
   var graph = Array(repeating: [Int](), count: n)
-  
+
   for wire in wires {
     graph[wire[0] - 1].append(wire[1] - 1)
     graph[wire[1] - 1].append(wire[0] - 1)
   }
   
-  for i in 0..<n {
-    for j in 0..<n {
-      if i != j {
-        let result = bfs(j, disabledNode: i)
-        answer = min(answer, abs(result - (n - result)))
+  func bfs(_ start: Int) -> Int {
+    var queue = [start]
+    var index = 0
+    
+    while index < queue.count {
+      let cur = queue[index]
+      index += 1
+      
+      visited[cur] = true
+      
+      for node in graph[cur] {
+        if visited[node] == false {
+          queue.append(node)
+        }
       }
     }
+    
+    return index
   }
   
-  return answer
+  var visited = [Bool]()
+  var result = Int.max
+  
+  for i in 0..<n {
+    visited = Array(repeating: false, count: n)
+    visited[i] = true
+    let count = bfs(0)
+    result = min(result, abs(count - (n - count)))
+  }
+  
+  return result
 }
