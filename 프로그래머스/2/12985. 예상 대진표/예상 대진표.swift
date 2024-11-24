4fc8f896e799637a0e93c8d7594a99423b2e1e60
DuDu <whqtkf12@naver.com>
2024-11-24 23:06:00 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 16 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.swift
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.swift
@@ -1,8 +1,7 @@
 import Foundation
 
 func solution(_ n:Int, _ a:Int, _ b:Int) -> Int {
-  var a = a, b = b
-  
+  var (a,b) = (a, b)
   var cnt = 0
   
   while true {
@@ -10,8 +9,8 @@ func solution(_ n:Int, _ a:Int, _ b:Int) -> Int {
     b = (b+1)/2
     cnt += 1
     
-    if a == b { return cnt }
+    if a == b { break }
   }
   
-  fatalError()
+  return cnt
 }
