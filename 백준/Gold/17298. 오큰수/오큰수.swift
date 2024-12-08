bad8b8edf4d053cc26bc6e64e824f5911323d421
DuDu <whqtkf12@naver.com>
2024-12-08 18:15:20 +0900

    [Gold IV] Title: 오큰수, Time: 572 ms, Memory: 133216 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.swift
+++ 백준/Gold/17298. 오큰수/오큰수.swift
@@ -1,19 +1,29 @@
-import Foundation
-
-let n = Int(readLine()!)!
-var input = readLine()!.split(separator: " ").map { Int(String($0))! }
-
-var stack = [Int]()
-var result = Array(repeating: -1, count: n)
-
-for index in 0..<n {
-  // 오큰수 발견하면 pop
-  while stack.isEmpty == false, input[stack.last!] < input[index] {
-    let target = stack.removeLast()
-    result[target] = input[index]
+func solution() -> String {
+  let n = Int(readLine()!)!
+  let numbers = readLine()!.split(separator: " ").map {Int($0)!}
+  
+  var stack = [(idx: Int, num: Int)]()
+  var ngeArr = Array(repeating: -1, count: n)
+  
+  for (index, number) in numbers.enumerated() {
+    if stack.isEmpty {
+      stack.append((index, number))
+      continue
+    }
+    
+    while true {
+      if stack.isEmpty == false, stack.last!.num < number {
+        let target = stack.removeLast()
+        ngeArr[target.idx] = number
+      } else {
+        stack.append((index, number))
+        break
+      }
+    }
   }
   
-  stack.append(index)
+  return ngeArr.map { String($0) }.joined(separator: " ")
 }
 
-print(result.map { String($0) }.joined(separator: " "))
+let result = solution()
+print(result)
