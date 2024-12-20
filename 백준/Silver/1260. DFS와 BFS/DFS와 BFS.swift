a28f6641ffdf15b12698f853d4f921ac1178c822
DuDu <whqtkf12@naver.com>
2024-12-20 13:45:23 +0900

    [Silver II] Title: DFS와 BFS, Time: 24 ms, Memory: 79952 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.swift
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.swift
@@ -1,55 +1,66 @@
-let inputs: [Int] = readLine()!.split{$0 == " "}.map{Int($0)!}
-let N = inputs[0]
-let M = inputs[1]
-let V = inputs[2]
+import Foundation
 
-var graph: [[Int]] = [[Int]](repeating: [], count: N+1)
-for _ in 0..<M {
-    let node: [Int] = readLine()!.split{$0 == " "}.map{Int($0)!}
-    graph[node[0]].append(node[1])
-    graph[node[1]].append(node[0])
-}
-
-func dfs(_ start: Int, _ graph: [[Int]]) -> String{
-    var visited: [Bool] = [Bool](repeating: false, count: N+1)
-    var stack: [Int] = [start]
-    var ans: String = ""
-    while !stack.isEmpty {
-        let node = stack.popLast()!
-        if visited[node] { continue }
-        
-        visited[node] = true
-        ans += "\(node) "
-        stack += graph[node]
-    }
-    return ans
-}
-
-func bfs(_ start: Int, _ graph: [[Int]]) -> String{
-    var ans: String = ""
-    var queueStartIndex: Int = 0
-    var queue: [Int] = [start]
-    var visited: [Bool] = [Bool](repeating: false, count: N+1)
+func bfs(start: Int, graph: [[Int]], visited: inout [Bool]) -> String {
+  visited[start] = true
+  
+  var log = [start + 1]
+  var index = 0
+  var queue = [start]
+  
+  while queue.count > index {
+    let cur = queue[index]
+    index += 1
     
-    while queueStartIndex<queue.count {
-        let node = queue[queueStartIndex]
-        queueStartIndex += 1
-        
-        if visited[node] {continue}
-        
-        visited[node] = true
-        ans += "\(node) "
-        queue += graph[node]
+    for next in graph[cur] where visited[next] == false {
+      log.append(next + 1)
+      visited[next] = true
+      queue.append(next)
     }
-    return ans
+  }
+  
+  return log.map {String($0)}.joined(separator: " ")
 }
 
-for i in 1...N {
-    graph[i].sort(by: >)
+func dfs(node: Int, graph: [[Int]], visited: inout [Bool]) -> String {
+  var stack = [node]
+  var log = [Int]()
+  
+  while !stack.isEmpty {
+    let cur = stack.removeLast()
+    
+    if visited[cur] == false {
+      log.append(cur + 1)
+      visited[cur] = true
+      stack += graph[cur].reversed()
+    }
+  }
+  
+  return log.map {String($0)}.joined(separator: " ")
 }
-print(dfs(V,graph))
-for i in 1...N{
-    graph[i].sort(by: <)
-}
-print(bfs(V,graph))
 
+func solution() -> String {
+  let nmv = readLine()!.split(separator: " ").map({ Int($0)! })
+  let (n, m, v) = (nmv[0], nmv[1], nmv[2] - 1)
+  
+  var graph = Array(repeating: [Int](), count: n)
+  var visited = Array(repeating: false, count: n)
+  
+  for _ in 0..<m {
+    let ab = readLine()!.split(separator: " ").map({ Int($0)! })
+    let (a, b) = (ab[0]-1, ab[1]-1)
+    graph[a].append(b)
+    graph[b].append(a)
+  }
+  
+  for i in 0..<n {
+    graph[i].sort()
+  }
+  
+  let dfsResult = dfs(node: v, graph: graph, visited: &visited)
+  visited = Array(repeating: false, count: n)
+  let bfsResult = bfs(start: v, graph: graph, visited: &visited)
+  
+  return "\(dfsResult)\n\(bfsResult)"
+}
+
+print(solution())
