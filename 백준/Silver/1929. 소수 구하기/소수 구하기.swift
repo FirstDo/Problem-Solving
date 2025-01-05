0f0a8907eb8103c66313b44298f04495e92e34ef
DuDu <whqtkf12@naver.com>
2025-01-05 12:20:24 +0900

    [Silver III] Title: 소수 구하기, Time: 28 ms, Memory: 82616 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.swift
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.swift
@@ -1,32 +1,34 @@
 import Foundation
 
-// 에라토스테네스의 체를 이용한 소수인지 여부 배열을 구함
 func makePrimes() -> [Bool] {
   let limit = 1_000_000
+  var prime = Array(repeating: true, count: limit+1)
+  prime[0...1] = [false, false]
   
-  var primes = Array(repeating: true, count: limit + 1)
-  primes[0] = false
-  primes[1] = false
-  
-  for i in 2 ..< Int(sqrt(Double(limit))) + 1 {
-    if primes[i] == true {
-      var j = 2
+  for i in 2...limit {
+    if prime[i] {
+      var j = i * 2
       
-      while i * j <= limit {
-        primes[i * j] = false
-        j += 1
+      while j <= limit {
+        prime[j] = false
+        j += i
       }
     }
   }
   
-  return primes
+  return prime
 }
 
-let input = readLine()!.components(separatedBy: " ").map { Int($0)! }
-let (m, n) = (input[0], input[1])
-
-let primes = makePrimes()
-
-for i in m...n where primes[i] {
-  print(i)
+func solution() -> String {
+  let mn = readLine()!.split(separator: " ").map {Int($0)!}
+  let (m, n) = (mn[0], mn[1])
+  
+  let primes = makePrimes()
+  
+  return (m...n)
+    .filter { primes[$0] }
+    .map { String($0) }
+    .joined(separator: "\n")
 }
+
+print(solution())
