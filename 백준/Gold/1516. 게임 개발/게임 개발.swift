9b07b9b408e1bb4b4555a768eea1fa7ae0c8e215
DuDu <whqtkf12@naver.com>
2025-01-23 17:35:10 +0900

    [Gold III] Title: 게임 개발, Time: 24 ms, Memory: 80132 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.swift
+++ 백준/Gold/1516. 게임 개발/게임 개발.swift
@@ -1,44 +1,79 @@
-let n = Int(readLine()!)!
+import Foundation
 
-var indegree = [Int](repeating: 0, count: n+1)
-var buildTime = [Int](repeating: 0, count: n+1)
-var dp = [Int](repeating: 0, count: n+1)
-var graph = [[Int]](repeating: [], count: n+1)
-var q: [Int] = []
+struct Queue<T> {
+  var elements: [T]
+  var index = 0
+  
+  init(_ elements: [T] = []) {
+    self.elements = elements
+  }
+  
+  var isEmpty: Bool {
+    !(elements.count > index)
+  }
+  
+  mutating func enqueue(_ element: T) {
+    elements.append(element)
+  }
+  
+  mutating func dequeue() -> T {
+    index += 1
+    return elements[index - 1]
+  }
+}
 
-for i in 1 ..< n+1 {
-    let lines = readLine()!.split(separator: " ").map{Int(String($0))!}
-
-    buildTime[i] = lines[0]
-    for l in lines[1...] {
-        if l == -1 {
-            break
-        }
-
-        indegree[i] += 1
-        graph[l].append(i)
-    } 
+func topologySort(_ cost: [Int], _ indegree: [Int], _ graph: [[Int]]) {
+  let n = graph.count
+  
+  var indegree = indegree
+  var queue = Queue<Int>()
+  var dp = Array(repeating: 0, count: n)
+  // dp[i] = i건물을 짓기 위해 필요한 최소시간
+  
+  for i in 1..<n {
     if indegree[i] == 0 {
-        q.append(i)
+      queue.enqueue(i)
     }
-}
-
-
-var idx = 0
-while idx < q.count {
-    let node = q[idx]
-    idx += 1
-
-    for nextNode in graph[node] {
-        dp[nextNode] = max(dp[nextNode],dp[node] + buildTime[node])
-
-        indegree[nextNode] -= 1
-        if indegree[nextNode] == 0 {
-            q.append(nextNode)
-        }
+  }
+  
+  while queue.isEmpty == false {
+    let cur = queue.dequeue()
+    
+    for next in graph[cur] {
+      indegree[next] -= 1
+      dp[next] = max(dp[next], dp[cur] + cost[cur])
+      
+      if indegree[next] == 0 {
+        queue.enqueue(next)
+      }
     }
+  }
+  
+  for i in 1..<n {
+    print(dp[i] + cost[i])
+  }
 }
 
-for i in 1..<n+1 {
-    print(dp[i]+buildTime[i])
+func solution() {
+  let n = Int(readLine()!)!
+  
+  var graph = Array(repeating: [Int](), count: n+1)
+  var cost = Array(repeating: 0, count: n+1)
+  var indegree = Array(repeating: 0, count: n+1)
+  
+  for i in 1...n {
+    let t = readLine()!.split(separator: " ").map { Int($0)! }
+    let preBuild = Array(t[1...].dropLast())
+    
+    cost[i] = t[0]
+    
+    for building in preBuild {
+      graph[building].append(i)
+      indegree[i] += 1
+    }
+  }
+  
+  topologySort(cost, indegree, graph)
 }
+
+solution()
