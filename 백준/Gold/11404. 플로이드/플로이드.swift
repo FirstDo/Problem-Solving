c55fb31d8e44417544b432cff52d92abe8135044
DuDu <whqtkf12@naver.com>
2025-01-25 14:25:53 +0900

    [Gold IV] Title: 플로이드, Time: 144 ms, Memory: 79648 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.swift
+++ 백준/Gold/11404. 플로이드/플로이드.swift
@@ -1,32 +1,44 @@
-let n = Int(readLine()!)!
-let inf = 100000000
+import Foundation
 
-var graph = Array(repeating: Array(repeating: inf, count: n), count: n)
+let INF = Int.max / 10
 
-for _ in 0..<Int(readLine()!)! {
-    let t = readLine()!.split(separator: " ").map{Int(String($0))!}
-    graph[t[0]-1][t[1]-1] = min(graph[t[0]-1][t[1]-1], t[2])
-}
-
-for i in 0..<n {
-    graph[i][i] = 0
-}
-
-for k in 0..<n {
-    for i in 0..<n {
-        for j in 0..<n {
-            if i == j {
-                continue
-            } else {
-                graph[i][j] = min(graph[i][k] + graph[k][j], graph[i][j])
-            }
+func floydWarshall(_ graph: [[Int]]) {
+  let n = graph.count - 1
+  var temp = graph
+  
+  for k in 1...n {
+    for i in 1...n {
+      for j in 1...n {
+        if i == j {
+          temp[i][j] = 0
+        } else {
+          temp[i][j] = min(temp[i][k] + temp[k][j], temp[i][j])
         }
+      }
     }
-}
-
-for i in 0..<n {
-    for j in 0..<n {
-        print(graph[i][j] >= inf ? 0 : graph[i][j], terminator: " ")
+  }
+  
+  for i in 1...n {
+    for j in 1...n {
+      print(temp[i][j] == INF ? 0 : temp[i][j], terminator: " ")
     }
     print()
+  }
 }
+
+func solution()  {
+  let n = Int(readLine()!)!
+  let m = Int(readLine()!)!
+  
+  var graph = Array(repeating: Array(repeating: INF, count: n+1), count: n+1)
+  
+  for _ in 0..<m {
+    let abc = readLine()!.split(separator: " ").map{Int($0)!}
+    let (a, b, c) = (abc[0], abc[1], abc[2])
+    graph[a][b] = min(graph[a][b], c)
+  }
+  
+  floydWarshall(graph)
+}
+
+solution()
